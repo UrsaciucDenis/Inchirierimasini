7bbb86338dfce5ace349c3555f25bc231dfee6c0
UrsaciucDenis <98519168+UrsaciucDenis@users.noreply.github.com>
2023-03-28 21:26:59 +0300

    Add files via upload

---
+++ Masina.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inchirierimasini
+{
+    public class Masina
+    {
+        private string marca;
+        private string model;
+        private int capacitatemotor;
+        private int pret;
+
+
+        //constructor implicit
+        public Masina()
+        {
+            marca = model = string.Empty;
+        }
+
+        //comstructor cu parametrii
+        public Masina(string marca, string model, int capacitatemotor, int pret)
+        {
+            this.marca = marca;
+            this.model = model;
+            this.capacitatemotor = capacitatemotor;
+            this.pret = pret;
+
+        }
+    }
+}
---
+++ Persoana.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inchirierimasini
+{
+    public class Persoana
+    {
+        private string nume;
+        private string prenume;
+        private int CNP;
+    }
+
+    //constructor implicit
+    public Persoana()
+    {
+        nume = prenume = string.Empty;
+    }
+    public Persoana (string nume, string prenume, int CNP)
+    {
+        this.nume = nume;
+        this.prenume = prenume;
+        this.CNP = CNP;
+    }
+
+
+}
